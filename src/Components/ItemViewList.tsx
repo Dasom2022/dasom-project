9df8eb55afa4ad2d450c6ac3c720ea61cb0ad87e
jooyeon Lee <ljyeon0713@naver.com>
2022-07-31 23:43:22 +0900

    fix: delete cart item

--- src/Components/ItemViewList.tsx
+++ src/Components/ItemViewList.tsx
@@ -5,207 +5,218 @@ import styled from "styled-components";
 import { item, itemDataVal, itemInfo, userInfoData } from "../atoms";
 
 const SelectedItem = styled.div`
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0px 10px;
-  border-bottom: 2px solid #bbbbbb;
-  & > img {
-    width: 80px;
-    height: 64px;
-  }
-  &:last-child {
-    border: none;
-  }
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0px 10px;
+    border-bottom: 2px solid #bbbbbb;
+    & > img {
+        width: 80px;
+        height: 64px;
+    }
+    &:last-child {
+        border: none;
+    }
 `;
 const SelectedItemInfo = styled.div`
-  height: 64px;
-  width: 100%;
-  display: flex;
-  padding-top: 10px;
-  box-sizing: border-box;
-  margin-left: 10px;
+    height: 64px;
+    width: 100%;
+    display: flex;
+    padding-top: 10px;
+    box-sizing: border-box;
+    margin-left: 10px;
 
-  align-items: center;
-  & > div:first-child {
-    width: 60%;
-    text-align: left;
-  }
-  & > div:nth-child(2) {
-    width: 20%;
-    text-align: center;
-  }
-  & > div:last-child {
-    width: 20%;
-    font-weight: bold;
-    text-align: right;
-    margin-right: 10px;
-  }
+    align-items: center;
+    & > div:first-child {
+        width: 60%;
+        text-align: left;
+    }
+    & > div:nth-child(2) {
+        width: 20%;
+        text-align: center;
+    }
+    & > div:last-child {
+        width: 20%;
+        font-weight: bold;
+        text-align: right;
+        margin-right: 10px;
+    }
 `;
 const PayBtn = styled.button`
-  background-color: transparent;
-  border: 2px solid white;
-  padding: 7px 15px;
-  border-radius: 22px;
-  font-weight: bold;
-  font-size: 18px;
-  color: white;
-  box-shadow: 0 4px 4px -4px black;
-  position: absolute;
-  top: 50%;
-  right: 0;
-  transform: translateX(-50%) translateY(-50%);
-  cursor: pointer;
+    background-color: transparent;
+    border: 2px solid white;
+    padding: 7px 15px;
+    border-radius: 22px;
+    font-weight: bold;
+    font-size: 18px;
+    color: white;
+    box-shadow: 0 4px 4px -4px black;
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateX(-50%) translateY(-50%);
+    cursor: pointer;
 `;
 const Bottom = styled.div`
-  height: 70px;
-  font-size: 27px;
-  background-color: #31a737;
-  display: flex;
-  align-items: center;
-  color: white;
-  position: relative;
-  padding: 0 20px;
+    height: 70px;
+    font-size: 27px;
+    background-color: #31a737;
+    display: flex;
+    align-items: center;
+    color: white;
+    position: relative;
+    padding: 0 20px;
 `;
 const Content = styled.div`
-  height: calc(100% - 140px);
-  overflow: scroll;
-  ::-webkit-scrollbar {
-    display: none;
-  }
+    height: calc(100% - 140px);
+    overflow: scroll;
+    ::-webkit-scrollbar {
+        display: none;
+    }
 `;
 const TotalCount = styled.div`
-  display: flex;
-  width: 25%;
-  justify-content: space-between;
-  & > span:last-child {
-    color: tomato;
-  }
+    display: flex;
+    width: 25%;
+    justify-content: space-between;
+    & > span:last-child {
+        color: tomato;
+    }
 `;
 const TotalPrice = styled(TotalCount)`
-  width: 40%;
-  margin-left: 15px;
-  & > span:last-child > span:last-child {
-    color: white;
-  }
+    width: 40%;
+    margin-left: 15px;
+    & > span:last-child > span:last-child {
+        color: white;
+    }
 `;
 const Pay = styled.div`
-  width: 30%;
-  height: 20vh;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  background-color: white;
-  border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  & > span {
-    font-weight: bold;
-  }
-  & > div {
-    width: 100%;
-    text-align: center;
-  }
-  button {
-    padding: 5px 8px;
-    border-radius: 20px;
-    border: 2px solid white;
-    color: white;
-    font-weight: bold;
-    box-shadow: 1px 4px 3px -3px #bbbbbb;
-    cursor: pointer;
-  }
-  button:first-child {
-    background-color: red;
-  }
-  button:last-child {
-    background-color: #31a737;
-  }
+    width: 30%;
+    height: 20vh;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    background-color: white;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    & > span {
+        font-weight: bold;
+    }
+    & > div {
+        width: 100%;
+        text-align: center;
+    }
+    button {
+        padding: 5px 8px;
+        border-radius: 20px;
+        border: 2px solid white;
+        color: white;
+        font-weight: bold;
+        box-shadow: 1px 4px 3px -3px #bbbbbb;
+        cursor: pointer;
+    }
+    button:first-child {
+        background-color: red;
+    }
+    button:last-child {
+        background-color: #31a737;
+    }
 `;
 function ItemViewList() {
-  const [itemDataValue, setItemDataValue] = useRecoilState(itemDataVal);
-  const [itemData, setItemData] = useRecoilState<any>(item);
-  const [payOpen, setPayOpen] = useState(false);
-  const [itemInfoS, setItemInfoS] = useRecoilState(itemInfo);
-  const navigate = useNavigate();
-  useEffect(() => {
-    let added = 0;
+    const [itemDataValue, setItemDataValue] = useRecoilState(itemDataVal);
+    const [itemData, setItemData] = useRecoilState<any>(item);
+    const [payOpen, setPayOpen] = useState(false);
+    const [itemInfoS, setItemInfoS] = useRecoilState(itemInfo);
+    const navigate = useNavigate();
+    useEffect(() => {
+        let added = 0;
 
-    if (itemDataValue.length != 0 && itemData.length == 0) {
-      setItemData([itemDataValue]);
-      return;
-    } else if (itemData.length != 0) {
-      for (let i = 0; i < itemData.length; i++) {
-        // if (itemData[i].itemCode === itemInfoS.itemCode) {
-        //   console.log("삭제");
-        //   setItemData(itemData.splice(i, 1));
-        // }
-        if (itemData[i].itemCode == itemDataValue.itemCode) {
-          let a = itemData;
-          let b = a.map((item: any, index: any) => {
-            if (index == i) {
-              return itemDataValue;
-            } else return item;
-          });
-          setItemData([...b]);
-          added = 1;
-          break;
+        if (itemDataValue.length != 0 && itemData.length == 0) {
+            setItemData([itemDataValue]);
+            return;
+        } else if (itemData.length != 0) {
+            for (let i = 0; i < itemData.length; i++) {
+                console.log(itemInfoS);
+                console.log(itemData);
+                // if (itemData[i].itemCode === itemInfoS.itemCode) {
+                //     console.log("삭제");
+                //     setItemData(itemData.splice(i, 1));
+                // }
+                if (itemData[i].itemCode == itemDataValue.itemCode) {
+                    let a = itemData;
+                    let b = a.map((item: any, index: any) => {
+                        if (index == i) {
+                            return itemDataValue;
+                        } else return item;
+                    });
+                    setItemData([...b]);
+                    added = 1;
+                    break;
+                }
+            }
+            if (!added) {
+                setItemData((prev: any) => [...prev, itemDataValue]);
+            }
         }
-      }
-      if (!added) {
-        setItemData((prev: any) => [...prev, itemDataValue]);
-      }
-    }
-  }, [itemDataValue]);
+    }, [itemDataValue]);
 
-  return (
-    <>
-      <Content>
-        {itemData.length > 0
-          ? itemData.map((item: any, index: any) => (
-              <SelectedItem key={index}>
-                <img
-                  src={process.env.PUBLIC_URL + `/image/${item.itemCode}.jpeg`}
-                />
-                <SelectedItemInfo>
-                  <div>{item.itemName}</div>
-                  <div>{item.count}개</div>
-                  <div>{item.price}원</div>
-                </SelectedItemInfo>
-              </SelectedItem>
-            ))
-          : null}
-      </Content>
-      <Bottom>
-        <TotalCount>
-          <span>수량 : </span>
-          <span>{itemInfoS?.totalCount}</span>
-        </TotalCount>
-        <TotalPrice>
-          <span>구매금액 : </span>
-          <span>
-            <span>{itemInfoS?.totalPrice}</span>
-            <span>원</span>
-          </span>
-        </TotalPrice>
-        <PayBtn onClick={() => setPayOpen(true)}>결제하기</PayBtn>
-      </Bottom>
+    return (
+        <>
+            <Content>
+                {itemData.length > 0
+                    ? itemData
+                          .filter((item: any) => item.count > 0)
+                          .map((item: any, index: any) => (
+                              <SelectedItem key={index}>
+                                  <img
+                                      src={
+                                          process.env.PUBLIC_URL +
+                                          `/image/${item.itemCode}.jpeg`
+                                      }
+                                  />
+                                  <SelectedItemInfo>
+                                      <div>{item.itemName}</div>
+                                      <div>{item.count}개</div>
+                                      <div>{item.price}원</div>
+                                  </SelectedItemInfo>
+                              </SelectedItem>
+                          ))
+                    : null}
+            </Content>
+            <Bottom>
+                <TotalCount>
+                    <span>수량 : </span>
+                    <span>{itemInfoS?.totalCount}</span>
+                </TotalCount>
+                <TotalPrice>
+                    <span>구매금액 : </span>
+                    <span>
+                        <span>{itemInfoS?.totalPrice}</span>
+                        <span>원</span>
+                    </span>
+                </TotalPrice>
+                <PayBtn onClick={() => setPayOpen(true)}>결제하기</PayBtn>
+            </Bottom>
 
-      {payOpen ? (
-        <Pay>
-          <span>결제하시겠습니까?</span>
-          <div>
-            <button onClick={() => setPayOpen(false)}>돌아가기</button>
-            <button onClick={() => navigate("/pay")}>결제하기</button>
-          </div>
-        </Pay>
-      ) : null}
-    </>
-  );
+            {payOpen ? (
+                <Pay>
+                    <span>결제하시겠습니까?</span>
+                    <div>
+                        <button onClick={() => setPayOpen(false)}>
+                            돌아가기
+                        </button>
+                        <button onClick={() => navigate("/pay")}>
+                            결제하기
+                        </button>
+                    </div>
+                </Pay>
+            ) : null}
+        </>
+    );
 }
 
 export default ItemViewList;
--- src/Routes/Main.tsx
+++ src/Routes/Main.tsx
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react";
 import ItemSearch from "../Components/ItemSearch";
 import { useRecoilState } from "recoil";
 import {
-  item,
-  itemDataVal,
-  itemInfo,
-  searchOpenState,
-  userInfoData,
+    item,
+    itemDataVal,
+    itemInfo,
+    searchOpenState,
+    userInfoData,
 } from "../atoms";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -16,155 +16,167 @@ import ItemViewList from "../Components/ItemViewList";
 import useInterval from "../hooks/useInterval";
 const Stomp = require("stompjs");
 const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  min-width: 620px;
+    width: 100%;
+    height: 100vh;
+    min-width: 620px;
 `;
 const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #31a737;
-  height: 70px;
-  padding: 0 15px;
-  color: white;
-  div > button {
-    margin: 0px 10px;
-    background-color: transparent;
-    border: 2px solid white;
-    padding: 7px 15px;
-    border-radius: 22px;
-    font-weight: bold;
-    font-size: 18px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #31a737;
+    height: 70px;
+    padding: 0 15px;
     color: white;
-    box-shadow: 0 4px 4px -4px black;
-    cursor: pointer;
-  }
+    div > button {
+        margin: 0px 10px;
+        background-color: transparent;
+        border: 2px solid white;
+        padding: 7px 15px;
+        border-radius: 22px;
+        font-weight: bold;
+        font-size: 18px;
+        color: white;
+        box-shadow: 0 4px 4px -4px black;
+        cursor: pointer;
+    }
 `;
 
 const Main = () => {
-  //소켓 기본 설정
-  let sock = new SockJS("https://43.200.61.12:3333/stomp");
-  let stomp = Stomp.over(sock);
-  const [searchOpen, setSearchOpen] = useRecoilState(searchOpenState);
-  const [userInfo, setUserInfo] = useRecoilState(userInfoData);
-  const [itemDataValue, setItemDataValue] = useRecoilState(itemDataVal);
-  const [itemData, setItemData] = useRecoilState<any>(item);
-  const [payOpen, setPayOpen] = useState(false);
-  const [itemInfoS, setItemInfoS] = useRecoilState(itemInfo);
-  const navigate = useNavigate();
-  const Logout = async (social: string) => {
-    if (social === "KAKAO") {
-      //카카오 로그아웃
-      const KAKAO_AUTH_URL_LOGOUT = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.REACT_APP_REST_API_KEY}&logout_redirect_uri=${process.env.REACT_APP_LOGOUT_REDIRECT_URI}`;
-      setUserInfo([]);
-      window.location.href = KAKAO_AUTH_URL_LOGOUT;
-    } else if (social === "NAVER") {
-      //네이버 로그아웃
-      //네이버 로그아웃 팝업창
-      let testPopUp: any;
-      const openPopUp = () => {
-        testPopUp = window.open(
-          "https://nid.naver.com/nidlogin.logout",
-          "_blank",
-          "toolbar=yes,scrollbars=yes,resizable=yes,width=1,height=1"
-        );
-      };
-      openPopUp();
-      setTimeout(() => testPopUp.close(), 1000);
-      setUserInfo([]);
-      navigate("/");
-    } else {
-      //기본 로그아웃
-      const token = localStorage.getItem("refreshToken");
-      axios
-        .post(`/api/member/auth/logout?refreshToken=${token}`)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          // ... 로그인 실패 처리
-          console.log(error);
+    //소켓 기본 설정
+    let sock = new SockJS("http://43.200.61.12:3333/stomp");
+    let stomp = Stomp.over(sock);
+    const [searchOpen, setSearchOpen] = useRecoilState(searchOpenState);
+    const [userInfo, setUserInfo] = useRecoilState(userInfoData);
+    const [itemDataValue, setItemDataValue] = useRecoilState(itemDataVal);
+    const [itemData, setItemData] = useRecoilState<any>(item);
+    const [payOpen, setPayOpen] = useState(false);
+    const [itemInfoS, setItemInfoS] = useRecoilState(itemInfo);
+    const navigate = useNavigate();
+    const Logout = async (social: string) => {
+        if (social === "KAKAO") {
+            //카카오 로그아웃
+            const KAKAO_AUTH_URL_LOGOUT = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.REACT_APP_REST_API_KEY}&logout_redirect_uri=${process.env.REACT_APP_LOGOUT_REDIRECT_URI}`;
+            setUserInfo([]);
+            window.location.href = KAKAO_AUTH_URL_LOGOUT;
+        } else if (social === "NAVER") {
+            //네이버 로그아웃
+            //네이버 로그아웃 팝업창
+            let testPopUp: any;
+            const openPopUp = () => {
+                testPopUp = window.open(
+                    "https://nid.naver.com/nidlogin.logout",
+                    "_blank",
+                    "toolbar=yes,scrollbars=yes,resizable=yes,width=1,height=1"
+                );
+            };
+            openPopUp();
+            setTimeout(() => testPopUp.close(), 1000);
+            setUserInfo([]);
+            navigate("/");
+        } else {
+            //기본 로그아웃
+            const token = localStorage.getItem("refreshToken");
+            axios
+                .post(`/api/member/auth/logout?refreshToken=${token}`)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    // ... 로그인 실패 처리
+                    console.log(error);
+                });
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            setUserInfo([]);
+            navigate("/");
+        }
+    };
+    useEffect(() => {
+        if (!userInfo?.username) {
+            navigate("/");
+            alert("로그인 필수!");
+        }
+        stomp.debug = null;
+        stomp.connect({}, () => {
+            stomp.subscribe(
+                `/sub/chat/read/${userInfo.username}`,
+                (data: any) => {
+                    console.log(JSON.parse(data.body).body);
+                    if (JSON.parse(data.body).body !== "wait") {
+                        //statusCodeValue
+                        const Data = JSON.parse(data.body);
+                        setItemDataValue(Data.body);
+                        console.log(Data.body);
+                    }
+                }
+            );
+
+            stomp.subscribe(
+                `/sub/item/weight/${userInfo.username}`,
+                (data: any) => {
+                    if (JSON.parse(data.body).body !== "wait") {
+                        const Data = JSON.parse(data.body);
+                        setItemInfoS(Data);
+                        console.log(Data);
+                        // if (Data.ifZero) {
+                        //   // setItemDataValue(Data.body);
+                        //   console.log(Data);
+                        // }
+                    }
+                }
+            );
         });
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-      setUserInfo([]);
-      navigate("/");
-    }
-  };
-  useEffect(() => {
-    if (!userInfo?.username) {
-      navigate("/");
-      alert("로그인 필수!");
-    }
-    stomp.debug = null;
-    stomp.connect({}, () => {
-      stomp.subscribe(`/sub/chat/read/${userInfo.username}`, (data: any) => {
-        // console.log(JSON.parse(data.body).body);
-        if (JSON.parse(data.body).body !== "wait") {
-          //statusCodeValue
-          const Data = JSON.parse(data.body);
-          setItemDataValue(Data.body);
-        }
-      });
+    }, []);
 
-      stomp.subscribe(`/sub/item/weight/${userInfo.username}`, (data: any) => {
-        if (JSON.parse(data.body).body !== "wait") {
-          const Data = JSON.parse(data.body);
-          setItemInfoS(Data);
-          // if (Data.ifZero) {
-          //   // setItemDataValue(Data.body);
-          //   console.log(Data);
-          // }
-        }
-      });
-    });
-  }, []);
-
-  useInterval(() => {
-    stomp.send(
-      `/pub/api/websocket/itemList/${userInfo.username}`,
-      {},
-      JSON.stringify({})
+    useInterval(() => {
+        stomp.send(
+            `/pub/api/websocket/itemList/${userInfo.username}`,
+            {},
+            JSON.stringify({})
+        );
+        stomp.send(
+            `/pub/api/websocket/itemWeight/${userInfo.username}`,
+            {},
+            JSON.stringify({})
+        );
+    }, 3000);
+    return (
+        <Container>
+            {userInfo?.username ? (
+                <>
+                    <Header>
+                        <div>
+                            <button
+                                onClick={() => {
+                                    setSearchOpen(true);
+                                }}
+                            >
+                                물건 검색
+                            </button>
+                            <button onClick={() => navigate("/itemcode")}>
+                                코드 상품추가
+                            </button>
+                        </div>
+                        <div>
+                            <span>{userInfo?.username}님 환영합니다!</span>
+                            <button onClick={() => navigate("/mypage")}>
+                                마이페이지
+                            </button>
+                            <button
+                                onClick={() => Logout(userInfo?.socialType)}
+                            >
+                                로그아웃
+                            </button>
+                        </div>
+                    </Header>
+                    <ItemViewList />
+                    {searchOpen ? <ItemSearch /> : null}
+                </>
+            ) : (
+                <></>
+            )}
+        </Container>
     );
-    stomp.send(
-      `/pub/api/websocket/itemWeight/${userInfo.username}`,
-      {},
-      JSON.stringify({})
-    );
-  }, 3000);
-  return (
-    <Container>
-      {userInfo?.username ? (
-        <>
-          <Header>
-            <div>
-              <button
-                onClick={() => {
-                  setSearchOpen(true);
-                }}
-              >
-                물건 검색
-              </button>
-              <button onClick={() => navigate("/itemcode")}>
-                코드 상품추가
-              </button>
-            </div>
-            <div>
-              <span>{userInfo?.username}님 환영합니다!</span>
-              <button onClick={() => navigate("/mypage")}>마이페이지</button>
-              <button onClick={() => Logout(userInfo?.socialType)}>
-                로그아웃
-              </button>
-            </div>
-          </Header>
-          <ItemViewList />
-          {searchOpen ? <ItemSearch /> : null}
-        </>
-      ) : (
-        <></>
-      )}
-    </Container>
-  );
 };
 export default Main;
